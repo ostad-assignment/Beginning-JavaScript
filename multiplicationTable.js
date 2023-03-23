ce50c12700ab43a549238e86a5ce01b96f8fb6f7
Jahid Hasan <jahid0676@gmail.com>
2023-03-23 12:52:38 +0600

    Update multiplicationTable.js

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,11 +1,11 @@
 function multiplicationTable(num) {
   if (Number.isInteger(num)) {
     for (let i = 1; i <= num; i++) {
-      console.log(`${num} x ${i} = ${num * i}`)
+      console.log(`${num} x ${i} = ${num * i}`);
     }
   } else {
-    console.log(`Argument should be integer. You provided: ${typeof num}`)
+    console.log(`Argument should be integer. You provided: ${typeof num}`);
   }
 }
 
-multiplicationTable(15)
+multiplicationTable(15);
