af456ea57700bfba1543dca4fef5d28cb3d125c5
Jahid Hasan <jahid0676@gmail.com>
2023-03-23 12:51:03 +0600

    Write a program that generates a multiplication table for a given number using a for loop.

---
+++ multiplicationTable.js
@@ -0,0 +1,11 @@
+function multiplicationTable(num) {
+  if (Number.isInteger(num)) {
+    for (let i = 1; i <= num; i++) {
+      console.log(`${num} x ${i} = ${num * i}`)
+    }
+  } else {
+    console.log(`Argument should be integer. You provided: ${typeof num}`)
+  }
+}
+
+multiplicationTable(15)
